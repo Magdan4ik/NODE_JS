f545f173d75b23b3453b1939bbcc8d301b462100
Magdan4ik <vladd0193@gmail.com>
2020-08-24 18:56:42 +0300

    12.0 Динамическое изменение корзины

--- public/app.js
+++ public/app.js
@@ -1,16 +1,49 @@
-document.querySelectorAll('.course-price').forEach(node => {
-	node.textContent = new Intl.NumberFormat('ru-RU', {
+
+const toCurrency = price => {
+	return 	new Intl.NumberFormat('ru-RU', {
 		currency: 'rub',
 		style: 'currency'
-	}).format(node.textContent)
+	}).format(price)
+}
+
+document.querySelectorAll('.course-price').forEach(node => {
+	node.textContent = toCurrency(node.textContent)
 })
 
 window.cart = {
+
+	container: document.getElementById('cart'),
+
 	async removeCourse(id) {
 		const res = await fetch('/cart/remove/' + id, {
 			method: 'delete',
 		})
 		const cart = await res.json()
-		return console.log(cart)
+
+		this.updateHTML(cart)
+	},
+
+	updateHTML(cart) {
+		if(cart.courses.length) {
+			const html = cart.courses.map(c => {
+				return `
+					<tr>
+						<td>${c.title}</td>
+						<td>${c.count}</td>
+						<td>
+							<button type="button" class="btn btn-small grey darken-4" onclick="window.cart.removeCourse('${c.id}')">
+								<i class="material-icons">close</i>
+							</button>
+						</td>
+					</tr>
+				`
+			}).join()
+
+			this.container.querySelector('tbody').innerHTML = html
+			this.container.querySelector('.course-price').textContent = toCurrency(cart.price)
+	
+		} else {
+			this.container.innerHTML = '<p>Корзина пуста</p>'
+		}
 	}
 }
