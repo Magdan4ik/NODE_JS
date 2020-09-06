9a1270448d30bbb4dbe9366ff62fcdd73459ae90
Magdan4ik <vladd0193@gmail.com>
2020-09-06 18:11:01 +0300

    3.0 Улучш. валидации

--- helpers/validators.js
+++ helpers/validators.js
@@ -2,23 +2,33 @@ const { body } = require('express-validator/check')
 const UserModel = require('../models/user')
 
 exports.registerValidators = [
-	body('email', 'Введите корректный email').isEmail().custom(async (value) => {
-		try {
-			const user = await UserModel.findOne({email: value})
+	body('email', 'Введите корректный email')
+		.isEmail()
+		.custom(async (value) => {
+			try {
+				const user = await UserModel.findOne({email: value})
 
-			if(user) {
-				return Promise.reject('Такой email уже занят')
+				if(user) {
+					return Promise.reject('Такой email уже занят')
+				}
+			} catch (error) {
+				console.log(error)
 			}
-		} catch (error) {
-			console.log(error)
-		}
-	}),
-	body('name', 'Имя должно состоять минимум из 3-х символов').isLength({min: 3}),
-	body('password', 'Пароль должен состоять минимум из 6-и символов').isLength({min: 6, max: 56}).isAlphanumeric(),
-	body('repassword').custom((value, {req}) => {
-		if(value !== req.body.password) {
-			throw new Error('Пароли должны совпадать')
-		}
-		return true
-	})
+		})
+		.normalizeEmail(),
+	body('name', 'Имя должно состоять минимум из 3-х символов')
+		.isLength({min: 3})
+		.trim(),
+	body('password', 'Пароль должен состоять минимум из 6-и символов')
+		.isLength({min: 6, max: 56})
+		.isAlphanumeric()
+		.trim(),
+	body('repassword')
+		.custom((value, {req}) => {
+			if(value !== req.body.password) {
+				throw new Error('Пароли должны совпадать')
+			}
+			return true
+		})
+		.trim()
 ]
